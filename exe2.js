6210968f503092cc3b5e0cc387d19d75f071b5b2
killerdias <143296784+killerdias@users.noreply.github.com>
2023-10-19 14:40:53 -0300

    Add files via upload

---
+++ exe2.js
@@ -0,0 +1,31 @@
+alert ("Bem vindos a lista 02");
+ diaDaSemana = prompt ('Qual dia da semana');
+ if (diaDaSemana == 'sabado'){
+    alert (" BOM FIM DE SEMANA");
+ } else if (diaDaSemana == 'domingo'){
+    alert("Bom fim de semana");
+    } else {
+        alert('Boa semana')
+    }
+ alert ('exe 02')
+ 
+ numero = prompt ('Escolha um numero');
+ if(numero > 0){
+    alert ('Numero Positivo')
+ }else {
+    alert('Numero negativo')
+ }   
+
+alert('exe03');
+pontos = prompt ('Informe os pontos');
+if(pontos >= 100){
+    alert('uhuuu você venceu')
+}else{
+    alert('Que pena tente novamente')
+}
+ let saldo = 500;
+ alert (`Seu saldo é  ${saldo}`);
+
+ let nome = prompt ('Qual seu nome')
+ alert (`Bem vindo a nossa lista de exe ${nome}`)
+
---
+++ exe3.js
@@ -0,0 +1,7 @@
+alert ('Bem vindo a lista 03')
+let numero = 1;
+while (numero <= 10) {
+    console.log(numero);
+    numero++;
+        
+}
